db6024538b77b192ef45fed2090951f3e414165e
unknown <SEU_EMAIL_DO_GITHUB>
2026-04-15 00:00:32 -0300

    alteração no branch develop

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Olá! Minha aplicação está rodando no Docker 🚀"
+    return "Aplicação DevOps com Docker funcionando 🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
